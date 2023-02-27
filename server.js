0629cd9d613fe9fd55189079d8db33d8724fe857
amandajoelle <dzukouamanda@gmail.com>
2023-02-27 12:18:09 +0100

    server Probe mit postman

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/prinseq', (req, res) => {
+
+    const file = req.body.file;
+
+    const input = req.body.input;
+
+    const command = "nextflow run" +file+" --input "+input;
+
+    //const command = "nextflow run nf-training/prinseq.nf";
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            res.status(500).send('Une erreur s\'est produite lors de l\'exécution de la commande');
+        } else {
+            res.send(stdout);
+        }
+    });
+});
+
+app.get('/nanofilt', (req, res) => {
+
+    //const file = req.body.file;
+
+    //const input = req.body.input;
+
+    //const command = "nextflow run" +file+" --input "+input;
+
+    const command = "nextflow run nf-training/prinseq.nf";
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            res.status(500).send('Une erreur s\'est produite lors de l\'exécution de la commande');
+        } else {
+            res.send(stdout);
+        }
+    });
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('Serveur démarré sur le port 3000');
+});
